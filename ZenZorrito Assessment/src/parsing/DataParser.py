b239dedea9104208927b5dd61074ee40f7e15abf
Óscar López <90772105+OscarLM32@users.noreply.github.com>
2023-10-12 19:15:24 +0200

    Initial data integrity implementation
    The current iteration of the algorithm can provide information on every field that is empty. It needs more curating to fulfill what is expeceted

--- ZenZorrito Assessment/src/parsing/DataParser.py
+++ ZenZorrito Assessment/src/parsing/DataParser.py
@@ -11,15 +11,37 @@ class DataParser:
         return users    
             
         
-
     def read_csv(self, file):
         #with -> try/catch and closes the file
         #I was having trouble with the encoding so I had to specifically define
         with open(file, 'r', encoding='utf_8') as csvfile:
             reader = csv.reader(csvfile)
             data = []
+            
             for row in reader:
                 data.append(row)
-            #this first line inside data contains the header, which we dont care about    
-            data.pop(0)        
+            
+            #saving the header for better logging
+            self.headers = data[0]
+            #we don't need the header inside the data
+            data.pop(0)     
+            self.check_data_integrity(data)            
             return data
+        
+
+    def check_data_integrity(self, data):
+        row_count = 1
+        for row in data:
+            if not row:
+                print("Hola")
+                continue
+            
+            for field in range(len(row)):
+                if row[field] in (None, ""):
+                    print('Row: ' + str(row_count) + ' Field: ' + self.headers[field] + ' empty')
+            
+            row_count += 1
+
+                
+                    
+        
