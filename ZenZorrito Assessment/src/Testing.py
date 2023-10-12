ba39bf4e9387dd6ac0b4eb084b2e9f207b51bd9e
Óscar López <90772105+OscarLM32@users.noreply.github.com>
2023-10-13 00:07:03 +0200

    Retrieving earliest check in customer data

--- ZenZorrito Assessment/src/Testing.py
+++ ZenZorrito Assessment/src/Testing.py
@@ -1,9 +1,14 @@
 import array
+from data_manipulation.DataManipulator import DataManipulator
 from parsing.DataParser import DataParser
 
 
 data_parser = DataParser()
 user_data = data_parser.parse_data("./data/data1.csv")
 
-for row in user_data:
-    print(row)
+#for row in user_data:
+    #print(row)
+
+data_manipulator = DataManipulator(user_data)
+
+print(data_manipulator.get_earliest_check_in_date_customer())
---
+++ ZenZorrito Assessment/src/data_manipulation/DataManipulator.py
@@ -0,0 +1,34 @@
+
+class DataManipulator:
+    
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.earliest_check_in_user = None
+        
+
+    def get_earliest_check_in_date_customer(self):
+        if self.earliest_check_in_user:
+            return self.earliest_check_in_user
+        else:
+            selected_customer = 0
+            earliest_date = self.dataset[selected_customer].last_check_in_date
+            
+            for i in range(1, len(self.dataset)):
+                if self.dataset[i].last_check_in_date < earliest_date:
+                    selected_customer = i
+                    earliest_date = self.dataset[i].last_check_in_date
+            
+            self.earliest_check_in_user = self.dataset[selected_customer]        
+            return self.earliest_check_in_user
+    
+
+    def get_latest_check_in_date_customer(self):
+        pass
+    
+    
+    def get_customer_names(self):
+        pass
+    
+
+    def get_customer_companies(self):
+        pass
