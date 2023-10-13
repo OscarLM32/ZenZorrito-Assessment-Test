47b8082103209c9991664f651f6c7a033ea40c76
Óscar López <90772105+OscarLM32@users.noreply.github.com>
2023-10-13 12:34:22 +0200

    Required fields logic
    Implemented the required fields logic. A set of mandatory fields can be specified in constructor. If one of these fields has an invalid value the row will be deleted. In case any other field has invalid value nothing happens, the program works as expected

--- ZenZorrito Assessment/src/Testing.py
+++ ZenZorrito Assessment/src/Testing.py
@@ -1,8 +1,9 @@
 import array
 from parsing.DataParser import DataParser
 
+mandatory_fields = ['Street', 'Zip', 'City', 'Last Check-In Date', 'Company']
 
-data_parser = DataParser()
+data_parser = DataParser(mandatory_fields)
 user_data = data_parser.parse_data("./data/data1.csv")
 
 for row in user_data:
